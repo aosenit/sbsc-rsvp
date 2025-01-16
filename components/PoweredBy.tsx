43aff647c4eae36052f1d598a7035f37ffa0cfe4
aosenit <creativitytraz22@gmail.com>
2025-01-16 16:56:27 +0100

    feat:add link to sbsc

--- components/PoweredBy.tsx
+++ components/PoweredBy.tsx
@@ -3,13 +3,18 @@ import logo from "@/assets/images/sbsclogo.png";
 
 export const PoweredBy = () => {
   return (
-    <div className="flex gap-1 items-center h-[50px]">
+    <a
+      href="https://www.sbsc.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex gap-1 items-center h-[50px]"
+    >
       <h3 className="text-lg text-[#595D62]">powered by</h3>
       <img
         src={logo.src}
         alt="logo"
         className="w-[60px] h-[40px] object-contain"
       />
-    </div>
+    </a>
   );
 };
