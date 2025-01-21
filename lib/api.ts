0d723848525822c07fecbe4c76dde4bb7e44ae80
aosenit <creativitytraz22@gmail.com>
2025-01-21 10:39:20 +0100

    change reg api

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-export const REGISTERURL = "invitee/register-for-event";
+export const REGISTERURL = "invitee/eventreg";
 export const AllMENUITEMSURL =
   "general/event/4d376275-be64-4eed-989d-20d76cc84498/menu-items";
 export const QRCODEURL = "general/event/qr_scan";
